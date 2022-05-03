f6550e3daad48aa262d6198514940c20705040a8
PredaKing2007 <91562271+PredaKing2007@users.noreply.github.com>
2022-05-03 15:08:27 +0300

    a

--- Incoming - Chapter 2/Assets/Scripts/AKM.cs
+++ Incoming - Chapter 2/Assets/Scripts/AKM.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-
+using System.Collections;
+using UnityEngine.UI;
 public class AKM : MonoBehaviour
 {
     private PlayerInput PlayerInput;
@@ -16,6 +17,10 @@ public class AKM : MonoBehaviour
     private Animator animator;
     private bool isScoped;
     private CameraRecoil cameraRecoil;
+    public Text Ammo;
+    public float MaxAmmo;
+    private float currentAmmo;
+    public float ReloadTime;
     [Space(10)]
     [Header("Camera Recoil")]
     public float RecoilX;
@@ -35,27 +40,38 @@ public class AKM : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentAmmo = MaxAmmo;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (PlayerInput.Player.Fire.IsPressed() && Time.time >= NextTimeToFire)
-        {
-            NextTimeToFire = Time.time + 1f / FireRate;
-            recoil.Fire();
-            Shoot();
-        }
         if (PlayerInput.Player.Scope.triggered)
         {
             isScoped = !isScoped;
             animator.SetBool("Scope", isScoped);
             recoil.aim = isScoped;
         }
+        if (currentAmmo == 0)
+        {
+            return;
+        }
+        if (PlayerInput.Player.Fire.IsPressed() && Time.time >= NextTimeToFire&&currentAmmo > 0)
+        {
+            NextTimeToFire = Time.time + 1f / FireRate;
+            recoil.Fire();
+            Shoot();
+        }
+
+        Ammo.text = currentAmmo.ToString();
+        if ((currentAmmo == 0) || (currentAmmo < MaxAmmo && PlayerInput.Player.Reload.triggered))
+        {
+            StartCoroutine(Reload());
+        }
     }
     void Shoot()
     {
+        currentAmmo--;
         cameraRecoil.RecoilFire();
         Vector3 mousePos = Vector3.zero;
         Vector2 ScreenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
@@ -70,4 +86,10 @@ public class AKM : MonoBehaviour
         }
         
     }
+    IEnumerator Reload()
+    {
+        Debug.Log("Reloading");
+        yield return new WaitForSeconds(ReloadTime);
+        currentAmmo = MaxAmmo;
+    } 
 }
